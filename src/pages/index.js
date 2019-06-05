2220769fe3f9bd694efd39d120cfc105123523f1
Meid <1556003277@mil>
2019-06-05 15:25:26 -0500

    testing content change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>have MRSI on me</h1>
+    <p></p>
+    <p></p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
