71c5e75153c91e01e47fcf8de3e0d4267278c790
Justin Meid <justin.r.meid@erdc.dren.mil>
2019-06-06 09:10:02 -0500

    created cos home page starting file

---
+++ src/pages/cos/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import Header from "../../components/header/header"
+
+class cosHome extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <Header />
+        This is the COS home page{" "}
+      </div>
+    )
+  }
+}
+
+export default cosHome
