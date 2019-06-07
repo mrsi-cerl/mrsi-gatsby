c35539e0d65c3a34654070c7e271045a94ea6b32
Justin Meid <justin.r.meid@erdc.dren.mil>
2019-06-07 15:11:56 -0500

    bootstrap navbar

--- src/components/header.js
+++ src/components/header.js
@@ -3,27 +3,69 @@ import React from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
+import { Helmet } from "react-helmet"
+import Dropdown from "react-bootstrap/Dropdown"
+import DropdownButton from "react-bootstrap/DropdownButton"
+import NavItem from "react-bootstrap/NavItem"
+import NavLink from "react-bootstrap/NavLink"
+
+function reduceChildRoutes({ props, items, page, depth }) {
+  if (page.children && page.children.length > 1) {
+  } else {
+  }
+}
 
 function Header(props) {
+  const SideNav = (
+    <Dropdown as={NavItem}>
+      <Dropdown.Toggle as={NavLink}>COS</Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item>Hello there!</Dropdown.Item>
+        <Dropdown as={NavItem} drop="right">
+          <Dropdown.Toggle as={NavLink}>Louisville</Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>Hello there!</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Dropdown.Menu>
+    </Dropdown>
+  )
+
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+      </Helmet>
+      <Navbar.Brand href="#home">MRSI</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
+          <NavDropdown title="COS">
+            <NavDropdown.Item>
+              <Link to="/cos">About COS</Link>
             </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
+            <NavDropdown.Item>
+              <Link to="/cos/army-standards">Army Standards</Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/cos/standard-designs">USACE Standard Designs</Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/cos/cos-poc">COS Points of Contact</Link>
             </NavDropdown.Item>
           </NavDropdown>
+          <Nav.Link>
+            <Link to="/crst">CRST</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/pdrs">PDRS</Link>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
