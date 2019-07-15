5adde371c633b8a058745634412d63352f5612e4
Justin Meid <justin.r.meid@erdc.dren.mil>
2019-07-15 15:00:35 -0500

    added list of accordions to tia page

--- src/pages/sustain/technology-in-action/index.js
+++ src/pages/sustain/technology-in-action/index.js
@@ -2,11 +2,33 @@ import React, { Component } from "react"
 import Layout from "../../../components/layout/layout"
 import GoogleMapReact from "google-map-react"
 import styles from "./TIA.module.css"
+import { Accordion, AccordionButton, AccordionContent } from "uswds-react"
+
+const getAllTIA = data => {
+  return data.allMarkdownRemark.edges.filter(
+    edge => edge.node.frontmatter.doc_type === "sustain_technology_in_action"
+  )
+}
+
+const getAllCategories = data => {
+  const cats = []
+  data.forEach(e => {
+    e.node.frontmatter.categories.forEach(cat => {
+      if (!cats.includes(cat)) {
+        cats.push(cat)
+      }
+    })
+  })
+  return cats
+}
 
 // Removing API key from GoggleMap component will give us development map
 // Leaving API key in there will give us production map but we have to
 // Whitelist localhost during development
-const TechnologyInAction = () => {
+const TechnologyInAction = ({ data }) => {
+  const tia = getAllTIA(data)
+  const cats = getAllCategories(tia)
+  console.log(cats)
   return (
     <Layout path="/sustain/technology-in-action/">
       <div className={styles.mapContainer}>
@@ -17,31 +39,41 @@ const TechnologyInAction = () => {
         />
       </div>
       <div className={styles.listContainer}>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
+        {cats.map((cat, idx) => (
+          <Accordion>
+            <AccordionButton defaultExpanded controls={`tia-section-${idx}`}>
+              {cat}
+            </AccordionButton>
+            <AccordionContent defaultHidden={false} id={`tia-section-${idx}`}>
+              {tia
+                .filter(e => e.node.frontmatter.categories.includes(cat))
+                .map(e => e.node.frontmatter.title)}
+            </AccordionContent>
+          </Accordion>
+        ))}
       </div>
     </Layout>
   )
 }
 export default TechnologyInAction
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            carousel_images {
+              publicURL
+            }
+            title
+            doc_type
+            project_coordinates
+            categories
+          }
+          html
+        }
+      }
+    }
+  }
+`
