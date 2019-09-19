98db22a154a3d6bd38454683711fb34a154bccdc
Heath D. Carr <heath.d.carr@gmail.com>
2019-09-19 12:10:15 -0500

    Try to execute js after page load.

--- src/html.js
+++ src/html.js
@@ -16,28 +16,15 @@ export default function HTML(props) {
             __html: `
             function detectIE() {
               var ua = window.navigator.userAgent;
-          
+
               var msie = ua.indexOf('MSIE ');
-              if (msie > 0) {
-                  // IE 10 or older => return version number
-                  return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-              }
-          
               var trident = ua.indexOf('Trident/');
-              if (trident > 0) {
-                  // IE 11 => return version number
-                  var rv = ua.indexOf('rv:');
-                  return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+              if (msie > 0 || trident > 0) {
+                alert("Please use a browser other than IE until we fix some problems");
               }
-          
-             
-              // other browser
-              return false;
           }
 
-          if(detectIE()){
-            alert("Please use a browser other than IE until we fix some problems");
-          }
+          window.onload = detectIE;
             `,
           }}
         />
