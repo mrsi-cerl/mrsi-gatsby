b333e390c7ab76d0f6fa6915307a3579babd05de
Justin Meid <Justin.R.Meid@erdc.dren.mil>
2019-10-02 10:45:02 -0500

    site wide alert improvements

--- src/components/layout/alert.js
+++ src/components/layout/alert.js
@@ -5,41 +5,47 @@ import moment from "moment"
 const Alert = () => {
   const data = useStaticQuery(graphql`
     query {
-      allSiteAlertJson {
-        edges {
-          node {
-            endDate
-            startDate
-            message
-          }
+      allAlertJson {
+        nodes {
+          startDate
+          message
+          endDate
+          level
+          title
         }
       }
     }
   `)
 
+  // do not render anything if server is building
   const isSSR = typeof window === "undefined"
-
   if (isSSR) {
     return null
   }
 
-  const { endDate, startDate, message } = data.allSiteAlertJson.edges[0].node
-
-  if (
-    moment().isAfter(startDate, "yyyy-mm-ddThh:mm:ss") &&
-    moment().isBefore(endDate, "yyyy-mm-ddThh:mm:ss")
-  ) {
-    return (
-      <div className="usa-alert usa-alert--info" style={{ marginBottom: 10 }}>
-        <div className="usa-alert__body">
-          <h3 className="usa-alert__heading">Announcement</h3>
-          <p className="usa-alert__text">{message}</p>
+  const createAlert = ({ endDate, startDate, message, level, title }) => {
+    if (
+      moment().isAfter(moment(startDate, "HH:mm  MM/DD/YYYY")) &&
+      moment().isBefore(moment(endDate, "HH:mm  MM/DD/YYYY"))
+    ) {
+      return (
+        <div
+          className={"usa-alert usa-alert--" + level}
+          style={{ marginBottom: 10 }}
+          key={endDate + message + startDate + level}
+        >
+          <div className="usa-alert__body">
+            <h3 className="usa-alert__heading">{title}</h3>
+            <p className="usa-alert__text">{message}</p>
+          </div>
         </div>
-      </div>
-    )
-  } else {
-    return null
+      )
+    } else {
+      return null
+    }
   }
+
+  return data.allAlertJson.nodes.map(e => createAlert(e))
 }
 
 export default Alert
