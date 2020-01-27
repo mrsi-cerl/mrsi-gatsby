2ec8b8a603108aef8171c143c2899893777d88ad
Justin Meid <Justin.R.Meid@erdc.dren.mil>
2020-01-27 13:09:03 -0600

    analytics

--- src/components/layout/helmet/helmet.js
+++ src/components/layout/helmet/helmet.js
@@ -1,25 +1,20 @@
 import React from "react"
 import { Helmet as ReactHelmet } from "react-helmet"
 
-const Helmet = ({ title }) => (
-  <ReactHelmet
-    defer={false}
-    title={title}
-    defaultTitle={"MRSI"}
-    titleTemplate={`%s | MRSI`}
-  >
-    <html lang="en" />
-    <meta
-      name="viewport"
-      content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
-    />
-    <script
-      async
-      type="text/javascript"
-      src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=DOD&subagency=USACE"
-      id="_fed_an_ua_tag"
-    ></script>
-  </ReactHelmet>
-)
+const Helmet = ({ title }) => {
+  return (
+    <ReactHelmet
+      defer={false}
+      defaultTitle={"MRSI"}
+      titleTemplate={`%s | MRSI`}
+    >
+      <html lang="en" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+      />
+    </ReactHelmet>
+  )
+}
 
 export default Helmet
