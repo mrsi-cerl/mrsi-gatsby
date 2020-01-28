6806d41969a5f2247410e6b637bb249747cefc57
Justin Meid <Justin.R.Meid@erdc.dren.mil>
2020-01-28 13:36:27 -0600

    moved search bar

--- src/components/layout/mrsi-banner/mrsi-banner2.js
+++ src/components/layout/mrsi-banner/mrsi-banner2.js
@@ -26,44 +26,6 @@ const MrsiBanner = () => {
               </p>
             </div>
           </div>
-          <div className={styles.search}>
-            <form
-              class="usa-search usa-search--small"
-              accept-charset="UTF-8"
-              action="https://search.usa.gov/search"
-              id="search_form"
-              method="get"
-            >
-              <div role="search">
-                <div>
-                  <input name="utf8" type="hidden" value="&#x2713;" />
-                </div>
-                <input
-                  id="affiliate"
-                  name="affiliate"
-                  type="hidden"
-                  value="mrsi"
-                />
-                <label class="usa-sr-only" for="search-field-small">
-                  Search
-                </label>
-                <input
-                  class="usa-input"
-                  id="search-field-small"
-                  type="search"
-                  name="query"
-                />
-                <button
-                  name="commit"
-                  value="Search"
-                  class="usa-button"
-                  type="submit"
-                >
-                  <span class="usa-sr-only">Search</span>
-                </button>
-              </div>
-            </form>
-          </div>
         </div>
         <div className={styles.menu}>
           <img src={hamMenu} alt="" />
