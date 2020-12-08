b07dd1702a4730cdda86a80a3d5fdb2b6e8d32e9
Heath D. Carr <heath.d.carr@gmail.com>
2020-12-08 12:06:18 -0600

    Current filelist iterating script

--- ListLibraryFiles.ps1
+++ ListLibraryFiles.ps1
@@ -1,27 +1,26 @@
-$root_path="C:\Users\RDCERJRM\Desktop\test-folder\"
-$save_location="C:\Users\RDCERJRM\Desktop\listfiles.json"
-
-
+$root_path="D:\inetpub\wwwroot\MRSI\"
+$save_location="C:\Users\hcarr\Desktop\s3ListBucket.json"
 
 $ls = Get-ChildItem -Path $root_path -Recurse | Select-Object -Property LastWriteTime, FullName, Name, Extension, Length
 
-
 $JSON = $( $ls | Where-Object{$_} | ForEach-Object{
 
-"{`n" + 
-"`"name`"`: `"$($_.Name)`"," + 
+"{`n" +
+"`"name`"`: `"$($_.Name)`"," +
 "`n" +
-"`"LastModified`"`: `"$($_.LastWriteTime)`"," + 
+"`"LastModified`"`: `"$($_.LastWriteTime)`"," +
 "`n" +
-"`"Key`"`: `"$($($_.FullName  -replace [Regex]::Escape($root_path), '') -replace '[\\]', '/')`"," + 
+"`"Key`"`: `"$($($_.FullName  -replace [Regex]::Escape($root_path), '') -replace '[\\]', '/')`"," +
 "`n" +
-"`"extension`"`: `"$($_.Extension)`"," + 
+"`"extension`"`: `"$($_.Extension)`"," +
 "`n" +
-"`"Size`"`: `"$($_.Length)`"" + 
+"`"Size`"`: `"$($_.Length)`"" +
 "`n" +
 "}"
 
 }) -join ",`n"
 
 
-$( "[ $JSON ]" ) -join ",`n" | Out-File -FilePath $save_location
+$( "[ $JSON ]" ) -join ",`n" | Out-File -Encoding ASCII -FilePath $save_location
+
+Write-Host "Done. The updated file is in $save_location"
