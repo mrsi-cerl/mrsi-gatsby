c604e16c2fd625677fc9e0d83d4d4fb982188e84
Jeff Morton <jeffrey.d.morton@erdc.dren.mil>
2022-12-14 15:58:20 -0600

    Added  flags: {     DEV_SSR: false   },   to turn off beta feature (for now)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  siteMetadata: {
+  flags: {
+    DEV_SSR: false
+  },
+    siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
