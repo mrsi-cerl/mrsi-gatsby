ecc09e1913bbe74bf33282ea08a2ee4a64ff4ea3
Jeff Morton <jeffrey.d.morton@erdc.dren.mil>
2022-12-15 10:40:54 -0600

    Code cleanup, ER number/link improvements

--- src/templates/coe-ctx-page.js
+++ src/templates/coe-ctx-page.js
@@ -5,29 +5,29 @@ import Library from "../components/library/library"
 import MrsiTable from "../components/mrsi-table"
 
 function getData(slug, data) {
-  // find the center that matches this pages slug
-  const center = data.allMarkdownRemark.edges.filter(
+  // find the center that matches this page's slug
+  const ctx = data.allMarkdownRemark.edges.filter(
     edge => edge.node.frontmatter.slug === slug
   )[0].node
 
   return {
-    center
+    ctx
   }
 }
 
 export default ({ data, pageContext }) => {
-  const { center } = getData(pageContext.slug, data);
-  // console.log(center)
+  const { ctx } = getData(pageContext.slug, data);
+  // console.log(ctx)
   const tableData = [
     {
         title: "Place of Center",
-        value: center.frontmatter.place_of_center,
+        value: ctx.frontmatter.place_of_center,
     },
     {
         title: "Website",
         value: (
-          <a href={center.frontmatter.website} target="_blank" rel="noopener noreferrer">
-            {center.frontmatter.website.length > 50 ? center.frontmatter.website.slice(0, 50) + "..." : center.frontmatter.website}
+          <a href={ctx.frontmatter.website} target="_blank" rel="noopener noreferrer">
+            {ctx.frontmatter.website.length > 50 ? ctx.frontmatter.website.slice(0, 50) + "..." : ctx.frontmatter.website}
           </a>
         )
     },
@@ -37,40 +37,40 @@ export default ({ data, pageContext }) => {
           <a  style={{textDecoration: 'none', color: 'black'}} 
             href={
               "tel:" +
-              (center.frontmatter.center_poc_phone_number)
+              (ctx.frontmatter.ctx_poc_phone_number)
             }
           >
-            {center.frontmatter.center_poc_name} <br/>
-            {center.frontmatter.center_poc_phone_number}
+            {ctx.frontmatter.ctx_poc_name} <br/>
+            {ctx.frontmatter.ctx_poc_phone_number}
           </a>
         ),
     },
     {
         title: "HQ POC",
-        value: center.frontmatter.hq_poc_name,
+        value: ctx.frontmatter.hq_poc_name,
     },
     {
         title: "HQ Div",
-        value: center.frontmatter.hq_division,
+        value: ctx.frontmatter.hq_division,
     },
     {
         title: "Date of Activation",
-        value: center.frontmatter.activation_date
+        value: ctx.frontmatter.activation_date
     },
     {
         title: "Recertification Date",
-        value: center.frontmatter.recertification_date
+        value: ctx.frontmatter.recertification_date
     },
     {
       title: "Functional Proponent (SES)",
-      value: center.frontmatter.center_functional_proponent,
+      value: ctx.frontmatter.center_functional_proponent,
     },
   ]
 
   return (
     <Layout path={pageContext.slug} MaxWidth={700} centerContent>
       <div style={{ paddingLeft: 20 }}>
-        <h1>{center.frontmatter.name_of_center}</h1>
+        <h1>{ctx.frontmatter.name_of_center}</h1>
         <div className="grid-row">
         <div className="tablet:grid-col" style={{ whiteSpace: 'nowrap' }}>
             <MrsiTable data={tableData} />
@@ -79,9 +79,9 @@ export default ({ data, pageContext }) => {
 
         <div
           className={"md"}
-          dangerouslySetInnerHTML={{ __html: center.html }}
+          dangerouslySetInnerHTML={{ __html: ctx.html }}
         />
-        <Library rootDir={center.frontmatter.file_library_root_path} />
+        <Library rootDir={ctx.frontmatter.file_library_root_path} />
       </div>
     </Layout>
   )
--- src/templates/coe-mcx-page.js
+++ src/templates/coe-mcx-page.js
@@ -5,29 +5,29 @@ import Library from "../components/library/library"
 import MrsiTable from "../components/mrsi-table"
 
 function getData(slug, data) {
-  // find the center that matches this pages slug
-  const center = data.allMarkdownRemark.edges.filter(
+  // find the center that matches this page's slug
+  const mcx = data.allMarkdownRemark.edges.filter(
     edge => edge.node.frontmatter.slug === slug
   )[0].node
 
   return {
-    center
+    mcx
   }
 }
 
 export default ({ data, pageContext }) => {
-  const { center } = getData(pageContext.slug, data);
-  // console.log(center)
+  const { mcx } = getData(pageContext.slug, data);
+  // console.log(mcx)
   const tableData = [
     {
         title: "Place of Center",
-        value: center.frontmatter.place_of_center,
+        value: mcx.frontmatter.place_of_center,
     },
     {
       title: "Website",
       value: (
-        <a href={center.frontmatter.website} target="_blank" rel="noopener noreferrer">
-            {center.frontmatter.website.length > 50 ? center.frontmatter.website.slice(0, 50) + "..." : center.frontmatter.website}
+        <a href={mcx.frontmatter.website} target="_blank" rel="noopener noreferrer">
+            {mcx.frontmatter.website.length > 50 ? mcx.frontmatter.website.slice(0, 50) + "..." : mcx.frontmatter.website}
         </a>
       )
     },
@@ -37,52 +37,56 @@ export default ({ data, pageContext }) => {
         <a  style={{textDecoration: 'none', color: 'black'}} 
           href={
             "tel:" +
-            (center.frontmatter.center_poc_phone_number)
+            (mcx.frontmatter.center_poc_phone_number)
           }
         >
-          {center.frontmatter.center_poc_name} <br/>
-          {center.frontmatter.center_poc_phone_number}
+          {mcx.frontmatter.center_poc_name} <br/>
+          {mcx.frontmatter.center_poc_phone_number}
         </a>
-      ),
+      )
   },
   {
         title: "HQ POC",
-        value: center.frontmatter.hq_poc_name,
+        value: mcx.frontmatter.hq_poc_name
     },
     {
         title: "HQ Div",
-        value: center.frontmatter.hq_division,
+        value: mcx.frontmatter.hq_division
     },
     {
         title: "ER Number",
         value: (
-          <a href={center.frontmatter.er_link} target="_blank" rel="noopener noreferrer">
-            {center.frontmatter.er_number}
-          </a>
+              mcx.frontmatter.er_number == "No ER estabished yet"
+                ?
+                  "No ER estabished yet"
+                :
+                  <a href={mcx.frontmatter.website} target="_blank" rel="noopener noreferrer">
+                    {mcx.frontmatter.er_number}
+                  </a>
         )
-      },
+        },
     {
         title: "Publication Date of ER",
-        value: center.frontmatter.er_publication_date
+        value: mcx.frontmatter.er_publication_date
     },
     {
         title: "Date of Activation",
-        value: center.frontmatter.activation_date
+        value: mcx.frontmatter.activation_date
     },
     {
         title: "Recertification Date",
-        value: center.frontmatter.recertification_date
+        value: mcx.frontmatter.recertification_date
     },
     {
       title: "Functional Proponent (SES)",
-      value: center.frontmatter.center_functional_proponent,
+      value: mcx.frontmatter.center_functional_proponent
     },
   ]
 
   return (
     <Layout path={pageContext.slug} MaxWidth={700} centerContent>
       <div style={{ paddingLeft: 20 }}>
-        <h1>{center.frontmatter.name_of_center}</h1>
+        <h1>{mcx.frontmatter.name_of_center}</h1>
         <div className="grid-row">
         <div className="tablet:grid-col" style={{ whiteSpace: 'nowrap' }}>
             <MrsiTable data={tableData} />
@@ -91,9 +95,9 @@ export default ({ data, pageContext }) => {
 
         <div
           className={"md"}
-          dangerouslySetInnerHTML={{ __html: center.html }}
+          dangerouslySetInnerHTML={{ __html: mcx.html }}
         />
-        <Library rootDir={center.frontmatter.file_library_root_path} />
+        <Library rootDir={mcx.frontmatter.file_library_root_path} />
       </div>
     </Layout>
   )
