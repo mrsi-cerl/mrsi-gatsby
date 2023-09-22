85f7ecf9546c104e13f711568bf100d91e39d98a
Jeff Morton <jeffrey.d.morton@erdc.dren.mil>
2023-09-22 18:09:15 -0500

    Missed one...

--- src/components/layout/navigation/side-nav/side-nav.tsx
+++ src/components/layout/navigation/side-nav/side-nav.tsx
@@ -82,6 +82,31 @@ function createNavList ( menuItems: any[], currPath: string ) {
   return menu;
 }
 
+function createPDRSNavList(p, currPath) {
+  return (
+    <div>
+    <li
+      className={cx("usa-sidenav__item", {
+        "usa-current": currPath,
+      })}
+    >
+      <Link to=".">About MCA/AFH Code2</Link>
+    </li>
+    <li class="usa-sidenav__item">
+      <Link to="https://www.wbdg.org/ffc/dod/unified-facilities-criteria-ufc/ufc-3-740-05" target="_blank" rel="noopener noreferrer"><b>CSRA Templates and Information</b><br/>
+      <small><i>The <b>UFC 3-740-05 Website</b> contains a checklist, the CSRA model template, CSRA Report template, and an example merged CSRA PDF report along with the UFC for CSRAs on MILCON projects.</i></small>
+      </Link>
+    </li>
+    <li class="usa-sidenav__item">
+      <Link to="https://usace.dps.mil/sites/KMP-CEC/SitePages/Cost-Tools.aspx" target="_blank" rel="noopener noreferrer"><b>Cost Risk Analysis: HQ Cost CoP website</b><br/>
+      <small><i>The HQ Cost CoP website contains a section for <b>Cost Risk Analysis</b> which contains links to the Civil Works guidance manual, CSRA template, ARA template, CSRA report (template + example merged CSRA PDF report), etc.<br/>
+      <b>***PLEASE NOTE***</b> this site is only accessible to USACE personnel.</i></small>
+      </Link>
+    </li>
+    </div>
+  );
+}
+
 function getSideNav ( pages: any[], path: string ) {
   let currPage: string = "";
   if ( path.includes( "/cos" ) ) {
@@ -94,6 +119,9 @@ function getSideNav ( pages: any[], path: string ) {
     currPage = "MODELRFP";
   } else if ( path.includes( "/sustain" ) ) {
     currPage = "SUSTAIN";
+  } else if ( path.includes( "/pdrs" ) ) {
+    currPage = "PDRS";
+    return createPDRSNavList( pages[currPage], path );
   } else {
     return null;
   }
