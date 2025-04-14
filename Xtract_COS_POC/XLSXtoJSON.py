9ba446489bf21f0cd874d15721f7a5f4c4ea958e
Jeff Morton <jeffrey.d.morton@erdc.dren.mil>
2025-04-14 15:46:12 -0500

    Python script to convert table in Excell to json

---
+++ Xtract_COS_POC/XLSXtoJSON.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+import sys
+from pathlib import Path
+from tkinter import *
+from tkinter import filedialog
+
+window = Tk()
+window.withdraw()
+infilepath = filedialog.askopenfilename(initialdir=".",
+                                        title="Select Excel File to convert",
+                                        filetypes= (("Excel Files","*.xlsx"),
+                                        ("all files","*.*")))
+inpathlength = (len(str(infilepath).strip()))
+#print(inpathlength)
+if inpathlength > 0:
+    # Figure out the json file name
+    ext = os.path.splitext(infilepath)[1]
+    #print(ext)
+    outfilepath = infilepath.replace(ext, '.json')
+    #print(outfilepath)
+
+    # Load the Excel file
+    excel_data = pd.read_excel(infilepath)
+
+    # Convert Excel data to JSON
+    json_data = excel_data.to_json(orient='records')
+
+    # Save JSON data to a file
+    with open(outfilepath, 'w') as json_file:
+        json_file.write(json_data)
+    print("Data file written to:" + outfilepath)
+else:
+    print ("No file chosen, aborting...")
