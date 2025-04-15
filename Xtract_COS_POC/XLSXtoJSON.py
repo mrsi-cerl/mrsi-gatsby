c118f796738f80fabb909cf89fb14caeb91fd236
Jeff Morton <jeffrey.d.morton@erdc.dren.mil>
2025-04-15 16:17:45 -0500

    Code cleanup and homage to Severance

--- Xtract_COS_POC/XLSXtoJSON.py
+++ Xtract_COS_POC/XLSXtoJSON.py
@@ -1,38 +1,29 @@
-import pandas as pd
+import pandas
 import os
-import sys
 import json
-import pprint
-from pathlib import Path
-from tkinter import *
 from tkinter import filedialog
 
-window = Tk()
-window.withdraw()
-infilepath = filedialog.askopenfilename(initialdir=".",
+inniefilepath = filedialog.askopenfilename(initialdir=".",
                                         title="Select Excel File to convert",
                                         filetypes= (("Excel Files","*.xlsx"),
                                         ("all files","*.*")))
-inpathlength = (len(str(infilepath).strip()))
-#print(inpathlength)
-if inpathlength > 0:
+if len(str(inniefilepath).strip()) > 0:  #Did they actually pick a file?
     # Figure out the json file name
-    ext = os.path.splitext(infilepath)[1]
-    #print(ext)
-    outfilepath = infilepath.replace(ext, '.json')
-    #print(outfilepath)
+    ext = os.path.splitext(inniefilepath)[1]
+    outtiefilepath = inniefilepath.replace(ext, '.json')
 
     # Load the Excel file
-    excel_data = pd.read_excel(infilepath)
+    excel_data = pandas.read_excel(inniefilepath)
 
     # Convert Excel data to JSON
     json_data = excel_data.to_json(orient='records')
 
     # Write formatted JSON data to a file
-    with open(outfilepath, 'w') as json_file:
+    with open(outtiefilepath, 'w') as json_file:
         json_file.write(
             json.dumps(json.loads(json_data), indent=2)
         )
-    print("Data file written to:" + outfilepath)
+    print("Data file written to:" + outtiefilepath)
+
 else:
     print ("No file chosen, aborting...")
